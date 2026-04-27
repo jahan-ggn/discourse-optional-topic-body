c7f9e01483d3b6d17b44afeb6482b4e95cea37a1
Jahan Gagan <jahanggn9@gmail.com>
2026-04-27 13:10:09 +0530

    Update plugin skeleton with plugin name

--- app/controllers/discourse_optional_topic_body/examples_controller.rb
+++ app/controllers/discourse_optional_topic_body/examples_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ::MyPluginModule
+module ::DiscourseOptionalTopicBody
   class ExamplesController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+DiscourseOptionalTopicBody::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::DiscourseOptionalTopicBody::Engine, at: "discourse-optional-topic-body" }
